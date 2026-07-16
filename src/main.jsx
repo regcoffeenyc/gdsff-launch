379dbd996ef57b44f5919e3eac431dbe8d7bce59
regcoffeenyc <regcoffee.nyc@gmail.com>
2026-07-16 09:12:10 +0400

    Replace HashRouter with BrowserSEO: BrowserRouter for real crawlable URLs (was HashRouter)Router in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import { Analytics } from '@vercel/analytics/react'
 import { SpeedInsights } from '@vercel/speed-insights/react'
 import App from './App'
@@ -8,10 +8,10 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter>
       <App />
       <Analytics />
       <SpeedInsights />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
 )
